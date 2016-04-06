cf370bb5568a88beb1e633ecae1a6b5d12b55b55
francoisdefever <fdefever.work@gmail.com>
2016-04-06 17:39:55 +0200

    add coolctrl in app.js | add just a scope out of the mainctrl

--- scripts/app.js
+++ scripts/app.js
@@ -3,4 +3,14 @@ angular.module("todoListApp", [])
     $scope.helloWorld = function() {
         console.log("Hello there! This is the helloWorld controller function, in the mainCtrl!");
     };
+})
+
+.controller('coolCtrl', function($scope){
+    $scope.whoAmI = function() {
+        console.log("Hello there, this is the coolCtrl function!");
+    };
+
+    $scope.helloWorld = function() {
+        console.log("This is not the maint ctrl!");
+    };
 });
