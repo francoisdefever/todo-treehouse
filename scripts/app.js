1052397c51a347a180c29f09ad4e4c2169d61325
francoisdefever <fdefever.work@gmail.com>
2016-04-07 11:37:34 +0200

    create todosjson in mock folder

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,14 @@
 angular.module("todoListApp", [])
 .controller('mainCtrl', function($scope, dataService) {
-    $scope.helloConsole = dataService.helloConsole;
     $scope.learningNgChange = function() {
         console.log("An input Changed");
     };
-
-    $scope.todos = [
-        {"name": "clean the house"},
-        {"name": "eat"},
-        {"name": "swim"},
-        {"name": "learn"},
-        {"name": "dance"},
-        {"name": "sleep"},
-    ];
+    
+    $scope.helloConsole = dataService.helloConsole;
 
 })
 
 .service('dataService', function() {
-
     this.helloConsole = function() {
         console.log("This is the hello console service");
     };
