0db8fab695d8e5ad1bbee18f5e80349b3d4e0f94
francoisdefever <fdefever.work@gmail.com>
2016-04-08 16:26:46 +0200

    add http-server with jitsu on http://127.0.0.1:8080 & http://192.168.1.66:8080

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,23 @@
 angular.module("todoListApp", [])
+
 .controller('mainCtrl', function($scope, dataService) {
     $scope.learningNgChange = function() {
         console.log("An input Changed");
     };
-    
-    $scope.helloConsole = dataService.helloConsole;
 
+    $scope.helloWorld = dataService.helloWorld;
+
+    $scope.todos = dataService.getTodos;
 })
 
-.service('dataService', function() {
-    this.helloConsole = function() {
+.service('dataService', function($http) {
+    this.helloWorld = function() {
         console.log("This is the hello console service");
     };
+
+    this.getTodos = $http.get('mock/todos.json')
+    .then(function(response) {
+        console.log(response.data);
+        return response.data;
+    });
 });
